b6d1d0fb94d220214556e883f59d7c5cf712b175
ibcooley <ben@cooley.ws>
2014-10-25 05:20:01 -0400

    Adding base for FPTree structure

---
+++ src/Algorithms/OrdersTable.java
@@ -0,0 +1,60 @@
+package Algorithms;
+
+import au.com.bytecode.opencsv.CSVReader;
+
+import javax.swing.table.AbstractTableModel;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by Ben on 10/25/2014.
+ */
+public class OrdersTable extends AbstractTableModel {
+
+    private String[] columnNames;
+    private List<String[]> rowData;
+
+    private OrdersTable() {
+        rowData = null;
+        columnNames = null;
+    }
+
+    public OrdersTable(String csvFileName) throws IOException {
+        this();
+        CSVReader reader = new CSVReader(new FileReader(csvFileName));
+        List<String[]> values = reader.readAll();
+        int cols = values.get(0).length;
+        columnNames = new String[cols];
+        for (int i = 0; i < cols; i++) {
+            columnNames[i] = values.get(0)[i];
+        }
+        for (int i = 1; i < values.size(); i++) {
+            rowData.add(values.get(i));
+        }
+    }
+
+    public String[] getColumnNames() {
+        return columnNames;
+    }
+
+    public List<String[]> getRowData() {
+        return rowData;
+    }
+
+    public int getColumnCount() {
+        return columnNames.length;
+    }
+
+    public int getRowCount() {
+        return rowData.size();
+    }
+
+    public String getColumnName(int col) {
+        return columnNames[col];
+    }
+
+    public String getValueAt(int row, int col) {
+        return rowData.get(row)[col];
+    }
+}
---
+++ src/Algorithms/RowColumnPair.java
@@ -0,0 +1,33 @@
+package Algorithms;
+
+/**
+ * Created by Ben on 10/25/2014.
+ */
+class RowColumnPair {
+
+    private final String column;
+    private final String row;
+
+    public RowColumnPair(String columnValue, String rowValue) {
+        this.column = columnValue;
+        this.row = rowValue;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        RowColumnPair that = (RowColumnPair) o;
+
+        return column.equals(that.column) && row.equals(that.row);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = column.hashCode();
+        result = 31 * result + row.hashCode();
+        return result;
+    }
+}
